177ae13a0fa879f64916c64e880f0886f3b6e506
liyalawal <victorialawal3.0@gmail.com>
2024-07-12 14:13:27 +0100

    classwork javascript in html

---
+++ week1/week2/dom.js
@@ -0,0 +1,26 @@
+// let newHeading= document.getElementById('heading');
+// newHeading.innerHTML='Welcome to DOM manipulation';
+// // .textContent does the same displays without tags
+// let newList= document.getElementById('list');
+// console.log(newList.textContent)
+// // inner.HTML displays the tags
+
+let newMainHeading = document.getElementById('mainHeading');
+let sub = document.getElementsByClassName('subheading');
+
+for (let i = 0; i < sub.length; i++) {
+    sub[i].innerHTML = 'Javascript is easy'
+
+}
+
+
+let lorem=document.getElementsByClassName('lorem');
+
+for(let i=0;i<lorem.length;i++){
+    lorem[i].innerHTML = 'Javascript is not easy'
+}
+let newh2=document.getElementsByTagName('h2');
+for (let i = 0; i < newh2.length; i++) {
+    newh2[i].innerHTML = 'Javascript go whine you but no panic'
+
+}
