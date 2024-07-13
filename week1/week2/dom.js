df33bfc0ea9ec6d577f8c53bb77e9893173508f3
liyalawal <victorialawal3.0@gmail.com>
2024-07-13 02:03:38 +0100

    how to writr in html with java

--- week1/week2/dom.js
+++ week1/week2/dom.js
@@ -14,13 +14,18 @@ for (let i = 0; i < sub.length; i++) {
 }
 
 
-let lorem=document.getElementsByClassName('lorem');
+let lorem = document.getElementsByClassName('lorem');
 
-for(let i=0;i<lorem.length;i++){
+for (let i = 0; i < lorem.length; i++) {
     lorem[i].innerHTML = 'Javascript is not easy'
 }
-let newh2=document.getElementsByTagName('h2');
+let newh2 = document.getElementsByTagName('h2');
 for (let i = 0; i < newh2.length; i++) {
     newh2[i].innerHTML = 'Javascript go whine you but no panic'
 
 }
+
+// +=adds to the other 
+let newElement = document.createElement('img');
+newElement.setAttribute('src', 'IMG_7736.PNG');
+lorem[1].appendChild(newElement);
