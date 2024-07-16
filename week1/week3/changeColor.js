67f89f58ccf8f8f45a0dc06c811cee9a20a7fd05
liyalawal <victorialawal3.0@gmail.com>
2024-07-16 13:53:10 +0100

    classwork';change background color change text color create fruit game

---
+++ week1/week3/changeColor.js
@@ -0,0 +1,18 @@
+let heading = document.getElementById('heading');
+let heading2= document.getElementById('heading2')
+
+heading.addEventListener('click', function () {
+    heading.style.color = 'green'
+    heading2.style.color = 'red'
+});
+
+   
+    
+    heading2.addEventListener('click', function () {
+    heading.style.color = 'red'
+    heading2.style.color = 'green'
+})
+
+   
+    
+
--- week1/week3/date.js
+++ week1/week3/date.js
@@ -18,3 +18,7 @@ function displayTime(){
 d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
 }
 setInterval(displayTime,1000)
+
+// learn to read  error messages
+// use console log to check part of ur Code 
+// run the code with both node.js amd the browser
