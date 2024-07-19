b1f7944ac147365252e442109ca6ac2b026574f6
liyalawal <victorialawal3.0@gmail.com>
2024-07-19 12:46:03 +0100

    rearrange folder structure for week three

---
+++ week1/week3/events/events.js
@@ -0,0 +1,13 @@
+// let heading =document.getElementById('test')
+
+// heading.addEventListener("click",()=>{
+// //     heading.style.color ="red";
+// // })
+
+// function changeToBlue(){
+//     heading.style.color="blue";
+// }
+
+// document.body.onload=function(){alert('when a page loads ')};
+document.body.onload=setTimeout()
+function sayHello(){}
